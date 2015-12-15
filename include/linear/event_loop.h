ee53e3b2a789983ff51b67f671c306b488b3ac5d
Norio Kobota <norio.kobota@jp.sony.com>
2015-12-15 21:02:39 +0900

    add doxy comment

--- include/linear/event_loop.h
+++ include/linear/event_loop.h
@@ -13,14 +13,22 @@ namespace linear {
 
 class EventLoopImpl;
 
+/**
+ * @class EventLoop event_loop.h "linear/event_loop.h"
+ * EventLoop class.
+ * enable to use specific event loop(a.k.a thread) at clients and servers
+ * @see linear::TCPServer, linear::TCPClient etc.
+ */
 class LINEAR_EXTERN EventLoop {
  public:
   static const EventLoop& GetDefault();
 
  public:
+  /// @cond hidden
   EventLoop();
   ~EventLoop();
   const linear::shared_ptr<linear::EventLoopImpl> GetImpl() const;
+  /// @endcond
 
  private:
   linear::shared_ptr<linear::EventLoopImpl> loop_;
